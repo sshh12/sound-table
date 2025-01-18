0aca6d12f4603a9f5319aa53f090134784c84845
Shrivu Shankar <shrivu1122@gmail.com>
2025-01-18 10:57:07 -0800

    init

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from db.database import init_db
+
+app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:8000",
+        "http://localhost:8080",
+        "https://*.up.railway.app",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    init_db()
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ frontend/src/context/user-context.js
@@ -0,0 +1,48 @@
+'use client';
+
+import { createContext, useContext, useEffect, useState } from 'react';
+import { api } from '@/lib/api';
+
+const UserContext = createContext({
+  user: null,
+  refreshUser: async () => {},
+});
+
+export function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem('sound-table-token')) {
+      api
+        .getCurrentUser()
+        .then(setUser)
+        .then(fetchData)
+        .catch((e) => {
+          if (e.message.includes('token')) {
+            localStorage.removeItem('sound-table-token');
+            window.location.href = '/';
+          }
+        });
+    } else {
+      api.createAccount().then(setUser).then(fetchData);
+    }
+  }, []);
+
+  const refreshUser = async () => {
+    const user = await api.getCurrentUser();
+    setUser(user);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        refreshUser,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export const useUser = () => useContext(UserContext);
